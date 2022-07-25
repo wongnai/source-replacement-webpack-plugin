2ea545ea43662025c30c76ca2c1f38ad89532aa2
Atipat Pankong <p.atipat@ext-lmwn.com>
2022-07-25 11:13:19 +0700

    fix conflict

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import InjectEntryPlugin from 'webpack-inject-entry-plugin'
 import Webpack from 'webpack'
 import AddAssetHtmlWebpackPlugin from 'add-asset-html-webpack-plugin'
 import path from 'path'
+import { Compiler } from 'webpack'
 
 const CODE_BLOCKER_SRC = path.resolve(
 	process.cwd(),
@@ -12,6 +12,31 @@ const CODE_BLOCKER_SRC = path.resolve(
 )
 
 const DEFAULT_ENTRY_NAME = 'client'
+
+function injectEntry(
+	options: Compiler['options'],
+	entryName: string,
+	injectFilepath: string,
+): void {
+	const entry: any =
+		typeof options.entry === 'function' ? options.entry() : Promise.resolve(options.entry)
+
+	options.entry = () =>
+		entry.then((e: any) => {
+			const injectEntry: typeof e[string] = e[entryName] || {}
+
+			if (!injectEntry.import) {
+				throw new Error(
+					`Could not find an entry named '${entryName}'. See https://webpack.js.org/concepts/entry-points/ for an overview of webpack entries.`,
+				)
+			}
+
+			if (!injectEntry.import.includes(injectFilepath)) {
+				injectEntry.import.unshift(injectFilepath)
+			}
+			return e
+		})
+}
 
 export class SourceReplacementPlugin {
 	private readonly entryName: string
@@ -29,6 +54,6 @@ export class SourceReplacementPlugin {
 			},
 		}).apply(compiler)
 
-		new InjectEntryPlugin({ entry: this.entryName, filepath: CODE_BLOCKER_SRC }).apply(compiler)
+		injectEntry(compiler.options, this.entryName, CODE_BLOCKER_SRC)
 	}
 }
