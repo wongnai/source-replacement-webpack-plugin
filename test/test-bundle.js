d37e2cf7663a0c0e52fea5f947c27e18e83f80f9
Atipat Pankong <p.atipat@ext-lmwn.com>
2022-07-26 17:11:06 +0700

    remove asset html plugin

--- test/test-bundle.js
+++ test/test-bundle.js
@@ -3,8 +3,6 @@ const path = require('path')
 
 const BUNDLE_FILE_NAME = 'bundle.js'
 
-const SOURCE_REPLACEMENT_FILE_NAME = 'index.js'
-
 const rootPath = process.cwd()
 const testPath = path.join(rootPath, 'test')
 const buildPath = path.join(testPath, 'build')
@@ -13,47 +11,52 @@ const htmlPath = path.join(testPath, 'build', 'index.html')
 const htmlSource = fs.readFileSync(htmlPath, { encoding: 'utf8' })
 
 function testBundlePreventExecuteSourceWithExistingSource() {
-    const outputScriptPath = path.join(buildPath, BUNDLE_FILE_NAME)
+	const outputScriptPath = path.join(buildPath, BUNDLE_FILE_NAME)
 
-    const outputText = fs.readFileSync(outputScriptPath, { encoding: 'utf8' })
+	const outputText = fs.readFileSync(outputScriptPath, { encoding: 'utf8' })
 
-    const SOURCE_CONTENT = './test/index.js'
-    
-    if (!outputText.includes(SOURCE_CONTENT)) {
-        throw new Error('No input source found in build')
-    }
+	const SOURCE_CONTENT = './test/index.js'
 
-    const PREVENT_RENDER_CONTENT = 'preventPerformExistingScriptDuringInjection()'
-    
-    if (!outputText.includes(PREVENT_RENDER_CONTENT)) {
-        throw new Error('No prevent render source found in build')
-    }
+	if (!outputText.includes(SOURCE_CONTENT)) {
+		throw new Error('No input source found in build')
+	}
 
-    if (outputText.indexOf(PREVENT_RENDER_CONTENT) > outputText.indexOf(SOURCE_CONTENT)) {
-        throw new Error('Invalid source order')
-    }
+	PREVENT_RENDER_CONTENT = 'preventPerformExistingScriptDuringInjection()'
 
-    if (!htmlSource.includes(BUNDLE_FILE_NAME)) {
-        throw new Error('No bundle source found in output')
-    }
+	const REQUIRE_SOURCE_REPLACEMENT =
+		'__webpack_require__("./node_modules/source-replacement/build/index.js")'
+
+	const REQUIRE_CODE_BLOCKER =
+		'__webpack_require__("./node_modules/source-replacement/build/code-blocker.js")'
+
+	const REQUIRE_MAIN_SCRIPT = '__webpack_require__("./test/index.js")'
+
+	if (!outputText.includes(PREVENT_RENDER_CONTENT)) {
+		throw new Error('No prevent render source found in build')
+	}
+
+	if (
+		outputText.indexOf(PREVENT_RENDER_CONTENT) > outputText.indexOf(SOURCE_CONTENT) &&
+		outputText.indexOf(SOURCE_CONTENT) > outputText.indexOf(REQUIRE_MAIN_SCRIPT)
+	) {
+		throw new Error('Invalid source order')
+	}
+
+	if (!htmlSource.includes(BUNDLE_FILE_NAME)) {
+		throw new Error('No bundle source found in output')
+	}
 }
 
 function testInjectSourceReplacementScript() {
-    const sourceReplacementPath = path.join(buildPath, SOURCE_REPLACEMENT_FILE_NAME)
+	const sourceReplacementPath = path.join(buildPath, BUNDLE_FILE_NAME)
 
-    const originalFilePath = path.join(rootPath, 'node_modules', 'source-replacement', 'build', 'index.js')
+	const sourceContent = fs.readFileSync(sourceReplacementPath, { encoding: 'utf8' })
 
-    const sourceContent = fs.readFileSync(sourceReplacementPath, { encoding: 'utf8' })
+	const FIRST_FUNCTION_REPLACEMENT_SCRIPT = 'getAugmentedNamespace(n)'
 
-    const sourceOrigin = fs.readFileSync(originalFilePath, { encoding: 'utf8' })
-
-    if (!sourceContent.includes(sourceOrigin)) {
-        throw new Error('Unmatched source replacement source')
-    }
-
-    if (!htmlSource.includes(`<script src="${SOURCE_REPLACEMENT_FILE_NAME}" type="module" async></script>`)) {
-        throw new Error('No bundle source found in output')
-    }
+	if (!sourceContent.includes(FIRST_FUNCTION_REPLACEMENT_SCRIPT)) {
+		throw new Error('Unmatched source replacement source')
+	}
 }
 
 testBundlePreventExecuteSourceWithExistingSource()
