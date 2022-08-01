040ccccf1acce0df99cb1ef9e7c8c4e028bf1757
Atipat Pankong <77198497+JAmoMES@users.noreply.github.com>
2022-08-01 11:50:36 +0700

    feat: use bundle more plugin (#7)
    * feature: 
    
    * use bundle more plugin

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,7 @@
 import Webpack from 'webpack'
-import { Compiler } from 'webpack'
+import { BundleMoreWebpackPlugin } from 'bundle-more-webpack-plugin'
 
 const DEFAULT_ENTRY_NAME = 'client'
-
-function injectEntry(
-	options: Compiler['options'],
-	entryName: string,
-	injectFilepath: string[],
-): void {
-	const entry: any =
-		typeof options.entry === 'function' ? options.entry() : Promise.resolve(options.entry)
-
-	options.entry = () =>
-		entry.then((e: any) => {
-			const injectEntry: typeof e[string] | undefined = e[entryName]
-
-			if (!injectEntry?.import) {
-				throw new Error(
-					`Could not find an entry named '${entryName}'. See https://webpack.js.org/concepts/entry-points/ for an overview of webpack entries.`,
-				)
-			}
-
-			injectEntry.import.unshift(...injectFilepath)
-
-			return e
-		})
-}
-
 export class SourceReplacementPlugin {
 	private readonly entryName: string
 
@@ -35,9 +10,11 @@ export class SourceReplacementPlugin {
 	}
 
 	apply(compiler: Webpack.Compiler) {
-		injectEntry(compiler.options, this.entryName, [
+		const sourceReplacementEntry = [
 			require.resolve('source-replacement'),
 			require.resolve('source-replacement/build/code-blocker.js'),
-		])
+		]
+
+		new BundleMoreWebpackPlugin(sourceReplacementEntry, this.entryName).apply(compiler)
 	}
 }
